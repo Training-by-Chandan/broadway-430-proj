af4e32b964230ce896aedd17ad54511eeb8d9a0c
Chandan Gupta Bhagat <chandan.bhagat@outlook.com>
2021-05-05 17:47:34 +0545

    Insert feature added

--- Broadway.Projects/Program.cs
+++ Broadway.Projects/Program.cs
@@ -9,43 +9,68 @@ namespace Broadway.Projects
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            string connectionString =
+          static  string connectionString =
             "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=Broadway_430;"
             + "Integrated Security=true";
 
+        static void Main(string[] args)
+        {
+            var result = "y";
+            do
+            {
+                //Console.WriteLine("Enter the table name:");
+                //string tableName = Console.ReadLine();
+                Console.WriteLine("Enter the Choice\n1: Display the record\n2: Create the reordd\n3: Update the record\n4: Delete the reccord\n");
+                using (SqlConnection connection =
+               new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    var choice = Console.ReadLine();
+                    switch (choice)
+                    {
+                        case "1":
+                            ReadFromTable(connection);
+                            break;
+                        case "2":
+                            InsertToTable(connection);
+                            break;
+                        case "3":
+                            UpdateToTable();
+                            break;
+                        case "4":
+                            DeleteFromTable();
+                            break;
+                        default:
+                            break;
+                    }
+
+                    connection.Close();
+                }
+                Console.WriteLine("Want to try more (y/n)?");
+                result = Console.ReadLine();
+            } while (result == "y" || result == "Y");
+
+            Console.ReadLine();
+
+        }
+
+        private static void ReadFromTable( SqlConnection connection)
+        {
             // Provide the query string with a parameter placeholder.
             string queryString =
                 "select * from Student";
 
-            // Specify the parameter value.
-            //int paramValue = 5;
-
-            // Create and open the connection in a using block. This
-            // ensures that all resources will be closed and disposed
-            // when the code exits.
-            using (SqlConnection connection =
-                new SqlConnection(connectionString))
-            {
                 // Create the Command and Parameter objects.
                 SqlCommand command = new SqlCommand(queryString, connection);
-                //command.Parameters.AddWithValue("@pricePoint", paramValue);
-
-                // Open the connection in a try/catch block.
-                // Create and execute the DataReader, writing the result
-                // set to the console window.
-                connection.Open();
+                
+                
                 try
                 {
                     SqlDataReader reader = command.ExecuteReader();
-
-                   
                     while (reader.Read())
                     {
                         var readers = new string[] { reader[0].ToString(), reader[1].ToString(), reader[2].ToString(), reader[3].ToString() };
-                        Console.WriteLine("\t{0}\t{1}\t\t{2}\t\t\t{3}\t{0}\t{0}",
-                            readers);
+                        Console.WriteLine(string.Join(",",readers));
                     }
                     reader.Close();
                 }
@@ -53,12 +78,42 @@ namespace Broadway.Projects
                 {
                     Console.WriteLine(ex.Message);
                 }
-                finally
-                {
-                    connection.Close();
-                }
+               
                 Console.ReadLine();
+            
+        }
+
+        private static void InsertToTable(SqlConnection connection)
+        {
+            Console.WriteLine("Enter Name");
+            var name = Console.ReadLine();
+            Console.WriteLine("Enter Email");
+            var email = Console.ReadLine();
+            Console.WriteLine("Enter Age");
+            var age = Console.ReadLine();
+
+            string queryString =
+               $"Insert into Student (Name, Email,Age) values ('{name}','{email}','{age}')";
+
+            // Create the Command and Parameter objects.
+            SqlCommand command = new SqlCommand(queryString, connection);
+            try
+            {
+                var res = command.ExecuteNonQuery();
             }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadLine();
+        }
+        private static void UpdateToTable()
+        {
+
+        }
+        private static void DeleteFromTable()
+        {
 
         }
     }
